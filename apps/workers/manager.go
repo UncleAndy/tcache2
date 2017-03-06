da8222d56fc627fe5858794165b90d758196adde
Andrey Velikoredchanin <andy@andyhost.ru>
2017-03-06 21:01:28 +0300

    Fix bottleneck in workers manager. Multitask architect for workers manager.

--- apps/workers/manager.go
+++ apps/workers/manager.go
@@ -10,6 +10,21 @@ import (
 	"syscall"
 	"os/signal"
 	"github.com/uncleandy/tcache2/log"
+	"time"
+	"github.com/uncleandy/tcache2/apps/loaders/sletat"
+	"sync"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+const (
+	EnvManagerFileConfig = "WORKERS_MANAGER_CONFIG"
+)
+
+var (
+	ForceStopManagerLoop = false
+	ToursBatchSize = int64(1000)
+	ManagerSettings worker_base.WorkerSettings
 )
 
 func InitManagers() {
@@ -42,7 +57,7 @@ func SignalsProcess(signals chan os.Signal) {
 
 	log.Info.Println("Detect stop command. Please, wait...")
 
-	worker_base.ForceStopManagerLoop = true
+	ForceStopManagerLoop = true
 }
 
 func main() {
@@ -52,9 +67,64 @@ func main() {
 	cache.InitFromEnv()
 	cache.RedisInit()
 	db.Init()
+
 	InitManagers()
 	InitManagersConfigs()
-	worker_base.RunManagerLoop()
+
+	LoadManagerConfig()
+	ManagerLoop()
 
 	log.Info.Println("Finished")
+}
+
+func ManagerLoop() {
+	println("Main loop start...")
+
+	wg := sync.WaitGroup{}
+	wg.Add(ManagerSettings.WorkerThreadsCount)
+
+	for i := 0; i < ManagerSettings.WorkerThreadsCount; i++ {
+		go func() {
+			for !ForceStopManagerLoop {
+				// Scan Redis tours loader queue & move tours to worker threads Redis queue
+				tours, err := cache.GetQueueBatch(sletat.LoaderQueueToursName, ToursBatchSize)
+				if err != nil || len(tours) == 0 {
+					time.Sleep(1 * time.Second)
+					continue
+				}
+
+				for _, tour_str := range tours {
+					for _, worker := range worker_base.Workers {
+						worker.SendTour(tour_str)
+					}
+				}
+			}
+
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	println("Main loop stoped.")
+}
+
+func  LoadManagerConfig() {
+	config_file := os.Getenv(EnvManagerFileConfig)
+	if config_file == "" {
+		log.Error.Fatalf("Partners tours worker config file name required (%s environment)", EnvManagerFileConfig)
+	}
+	_, err := os.Stat(config_file)
+	if os.IsNotExist(err) {
+		log.Error.Fatalf("Partners tours worker config file '%s' not exists.", config_file)
+	}
+
+	dat, err := ioutil.ReadFile(config_file)
+	if err != nil {
+		log.Error.Fatalln(err)
+	}
+
+	err = yaml.Unmarshal(dat, &ManagerSettings)
+	if err != nil {
+		log.Error.Fatalf("error: %v", err)
+	}
 }
--- apps/workers/worker_base/worker_base.go
+++ apps/workers/worker_base/worker_base.go
@@ -1,11 +1,5 @@
 package worker_base
 
-import (
-	"time"
-	"github.com/uncleandy/tcache2/cache"
-	"github.com/uncleandy/tcache2/apps/loaders/sletat"
-)
-
 type WorkerBaseInterface interface {
 	Init()
 	Stop()
@@ -26,8 +20,6 @@ type WorkerSettings struct {
 
 var (
 	Workers []WorkerBaseInterface
-	ForceStopManagerLoop = false
-	ToursBatchSize = int64(1000)
 )
 
 func RunWorkers() {
@@ -37,31 +29,9 @@ func RunWorkers() {
 	}
 }
 
-func RunManagerLoop() {
-	ManagerLoop()
-}
-
 func WaitWorkersFinish() {
 	for _, worker := range Workers {
 		worker.WaitFinish()
 	}
 }
 
-func ManagerLoop() {
-	// Scan Redis tours loader queue & move tours to worker threads Redis queue
-	println("Main loop start...")
-	for !ForceStopManagerLoop {
-		tours, err := cache.GetQueueBatch(sletat.LoaderQueueToursName, ToursBatchSize)
-		if err != nil || len(tours) == 0 {
-			time.Sleep(1 * time.Second)
-			continue
-		}
-
-		for _, tour_str := range tours {
-			for _, worker := range Workers {
-				worker.SendTour(tour_str)
-			}
-		}
-	}
-	println("Main loop stoped.")
-}
